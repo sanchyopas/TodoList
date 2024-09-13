778ae9199c2bba5cb59c11aabbbb2cf8e27d3e81
sanchyopas <saniagolovanev@gmail.com>
2024-09-14 00:45:37 +0700

    feat: develop universal input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,52 @@
 import {ChangeEvent, useState} from "react";
 import "./App.css";
 import {Button} from "./components/Button/Button";
+import {Input} from "./components/Input/Input";
+import {v1} from "uuid";
 
 function App() {
+  // state input add task
   const [value, setValue] = useState("");
 
+  // changed state input value
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    console.log(event);
+
     setValue(event.currentTarget.value);
+  };
+
+  const [tasks, setTask] = useState([
+    {id: v1(), title: "REACT", isDone: true},
+    {id: v1(), title: "REDUX", isDone: false},
+    {id: v1(), title: "CSS", isDone: true},
+    {id: v1(), title: "REACT ROUTING", isDone: false},
+  ]);
+
+  const addTask = () => {};
+
+  const [parag, setParag] = useState([{id: v1(), text: "hello your first text"}]);
+
+  const addParagraph = () => {
+    console.log(value);
+
+    setParag([...parag, {id: v1(), text: value}]);
+    setValue("");
   };
 
   return (
     <div className="App">
       <div className="add-task-inner">
-        <input type="text" className="input-add" placeholder={"Add task"} onChange={onChangeHandler} />
-        <Button className={"add-task-btn"} title={"+"} />
+        <Input onChange={onChangeHandler} className="input-add" placeholder={"Add task"} value={value} />
+        {/* <input type="text"  placeholder={"Add task"} onChange={onChangeHandler} /> */}
+        <Button className={"add-task-btn"} title={"+"} onClick={addParagraph} />
       </div>
-      <p>{value}</p>
+      {parag.map((p) => {
+        return (
+          <p>
+            {p.id} - {p.text}
+          </p>
+        );
+      })}
     </div>
   );
 }
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,9 @@
-type InputPropsType = {};
-export const Input = (props: InputPropsType) => {
-  return <div></div>;
+type InputPropsType = {
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  className?: string;
+  value: string;
+};
+export const Input = ({onChange, placeholder, className, value}: InputPropsType) => {
+  return <input type="text" onChange={onChange} className={className} placeholder={placeholder} value={value} />;
 };
